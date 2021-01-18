338517d92207ead072eb7dbb1660d636f2cd48a8
Bartek <bartektomaszczuk@gmail.com>
2021-01-18 11:29:20 +0100

    Commit 1

---
+++ Zliczanie/src/zliczanie/Zliczanie.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zliczanie;
+
+/**
+ *
+ * @author barte
+ */
+public class Zliczanie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
